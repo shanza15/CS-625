fa4d15d756c8b7af2988f79c62361dcfd77714a3
shanza15 <shanzashanza19@gmail.com>
2019-03-24 13:38:04 +0500

    g1

---
+++ Guess3.java
@@ -0,0 +1,15 @@
+class Guess3 {
+ public static void main(String args[])
+   throws java.io.IOException {
+   char ch, answer = 'K';
+   System.out.println("I'm thinking of a letter between A to Z.");
+   System.out.print("Can you guess it: ");
+   ch = (char) System.in.read();
+   if(ch == answer) System.out.println("** Right **");
+   else {
+      System.out.print(". . .Sorry, you're ");
+      if(ch < answer) System.out.println("too low");
+      else System.out.println("too high");
+        }
+     }
+ }
