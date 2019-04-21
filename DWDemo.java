4ac50f7c1d960f23814984970cec739ce763bb11
shanza15 <shanzashanza19@gmail.com>
2019-04-21 15:05:31 +0500

    programs

---
+++ DWDemo.java
@@ -0,0 +1,10 @@
+class DWDemo {
+  public static void main(String args[])
+    throws java.io.IOException {
+     char ch;
+    do {
+      System.out.print("Press a key followed by ENTER: ");
+      ch = (char) System.in.read();
+      } while(ch != 'q');
+    } 
+}
---
+++ Guess4.java
@@ -0,0 +1,21 @@
+class Guess4 {
+ public static void main(String args[])
+   throws java.io.IOException {
+   char ch, ignore, answer = 'K';
+  do {
+   System.out.println("I'm thinking of a letter between A to Z.");
+   System.out.print("Can you guess it: ");
+   ch = (char) System.in.read();
+  do {
+    ignore = (char) System.in.read();
+     } while(ignore != '\n');
+   if(ch == answer) System.out.println("** Right **");
+   else {
+      System.out.print(". . .Sorry, you're ");
+      if(ch < answer) System.out.println("too low");
+      else System.out.println("too high");
+      System.out.println("Try again!\n");
+    }
+  } while(answer != ch);
+     }
+ }
